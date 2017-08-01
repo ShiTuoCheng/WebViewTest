cf893950d69f944b5853171f32192adce5fd5fc7
Shi Tuocheng <shituocheng@126.com>
2017-08-01 12:44:41 +0800

    void 0

---
+++ app/src/main/java/com/shituocheng/stcdribbble/webviewtest/MainActivity.java
@@ -0,0 +1,69 @@
+package com.shituocheng.stcdribbble.webviewtest;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.JsResult;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+    private TextView logTextView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.webview);
+        // 启用javascript
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        // 从assets目录下面的加载html
+        mWebView.loadUrl("file:///android_asset/javascript.html");
+        mWebView.addJavascriptInterface(this, "wx");
+        logTextView = (TextView) findViewById(R.id.text);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                // 无参数调用
+                mWebView.loadUrl("javascript:actionFromNative()");
+                // 传递参数调用
+                mWebView.loadUrl("javascript:actionFromNativeWithParam(" + "'come from Native'" + ")");
+            }
+        });
+
+    }
+
+    @android.webkit.JavascriptInterface
+    public void actionFromJs() {
+        mWebView.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "js调用了Native函数", Toast.LENGTH_SHORT).show();
+                String text = logTextView.getText() + "\njs调用了Native函数";
+                logTextView.setText(text);
+            }
+        });
+    }
+
+    @android.webkit.JavascriptInterface
+    public void actionFromJsWithParam(final String str) {
+        mWebView.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "js调用了Native函数传递参数：" + str, Toast.LENGTH_SHORT).show();
+                String text = logTextView.getText() +  "\njs调用了Native函数传递参数：" + str;
+                logTextView.setText(text);
+            }
+        });
+
+    }
+}
